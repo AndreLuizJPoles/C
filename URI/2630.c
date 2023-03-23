a52a50594add6520e30ef0c412ffe2200b320377
André Luiz J Poles <110946640+AndreLuizJPoles@users.noreply.github.com>
2023-03-23 17:45:24 -0300

    Update 2630.c

--- URI/2630.c
+++ URI/2630.c
@@ -16,14 +16,15 @@ int main(void)
 	int i, chamadas, resultado;
 	RGB cores;
 	char tipo[5];
-	
+	//Entrada do numero de vezes que será feita a operação
 	scanf("%d", &chamadas);
 	
 	for(i=0;i<chamadas;i++)
 	{
+	//Entrada do tipo e das números de vermelho, verde e azul
 		scanf("%4s", tipo);
 		scanf("%d %d %d", &cores.R, &cores.G, &cores.B);
-		
+	//Verificando qual é o tipo e chamando a função de cada tipo
 		if(strcmp(tipo, "eye")==0)
 		{
 			resultado = eye(cores);
@@ -40,22 +41,23 @@ int main(void)
 		{
 			resultado = max(cores);
 		}
-		
+	//Impressão do resultado com a contagem de casos
 		printf("Caso #%d: %d\n", i+1, resultado);
 	}
 	
 	return 0;
 }
-
+//Função eye
 int eye(RGB cor)
 {	
 	return 0.3*cor.R + 0.59*cor.G + 0.11*cor.B;
 }
-
+//Função mean
 int mean(RGB cor)
 {
 	return (cor.R+cor.G+cor.B)/3;
 }
+//Função min
 int min(RGB cor)
 {
 	if(cor.R <= cor.G && cor.R <= cor.B)
@@ -65,6 +67,7 @@ int min(RGB cor)
 	else
 		return cor.B;
 }
+//Função max
 int max(RGB cor)
 {
 	if(cor.R >= cor.G && cor.R >= cor.B)
